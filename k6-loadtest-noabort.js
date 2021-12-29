afc02204e690b2aa39315059797604c5b7e844b5
Sundar K <sundar_babu@hotmail.com>
2021-12-29 11:36:49 -0600

    added k6-loadtest-noabort.js, updated Jenkinsfile-underdev-dec29-2021

---
+++ k6-loadtest-noabort.js
@@ -0,0 +1,27 @@
+import http from "k6/http";
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: "30s", target: 25 },
+    { duration: "1m", target: 50 },
+    { duration: "20s", target: 0 },
+  ],
+};
+
+export default function () {
+  const pages = [
+    "/",
+    "/posts/youtube/",
+    "/posts/catalog/",
+    "/this-does-not-exist/",
+  ]
+  for (const page of pages) {
+    const res = http.get("https://www.yahoo.com" + page);
+    check(res, {
+      "status was 200": (r) => r.status == 200,
+      "duration was <= ": (r) => r.timings.duration <= 200
+    });
+    sleep(1);
+  }
+}
